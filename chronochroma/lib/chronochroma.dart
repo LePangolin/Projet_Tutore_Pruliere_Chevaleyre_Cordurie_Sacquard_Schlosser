7811f9fdbc8ea37d535fc987db73cdb3943904b9
LePangolin <91209500+LePangolin@users.noreply.github.com>
2022-12-16 10:30:17 +0100

    build(Files): réarrangement des fichiers

---
+++ chronochroma/lib/chronochroma.dart
@@ -0,0 +1,50 @@
+import 'package:flame_tiled/flame_tiled.dart';
+import 'package:chronochroma/helpers/directions.dart';
+import 'package:chronochroma/player.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'helpers/navigation_keys.dart';
+
+class Chronochroma extends FlameGame {
+  final Player player = Player();
+  double gravity = 1.5;
+  Vector2 velocity = Vector2(0, 0);
+  late TiledComponent homeMap;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    homeMap = await TiledComponent.load('map.tmx', Vector2.all(32));
+    add(homeMap);
+    add(player);
+  }
+
+  onArrowKeyChanged(Direction direction) {
+    player.direction = direction;
+  }
+
+
+
+  static MaterialApp createGame() {
+    final game = Chronochroma();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GameWidget(
+              game: game
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: NavigationKeys(
+                onDirectionChanged: game.onArrowKeyChanged,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
