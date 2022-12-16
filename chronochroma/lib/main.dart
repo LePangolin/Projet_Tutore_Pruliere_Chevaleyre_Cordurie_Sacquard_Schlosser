7e5bd1fe7d40fd8ce91f7dea4d33e97a77d9dab9
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2022-12-16 18:00:08 +0100

    feat(upgrade): début page

--- chronochroma/lib/main.dart
+++ chronochroma/lib/main.dart
@@ -1,25 +1,18 @@
 import './chronochroma.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-
-
+import 'upgrade.dart';
 
 void main() {
   // On s'assure que le binding est initialisé
   WidgetsFlutterBinding.ensureInitialized();
   // On désactive la rotation de l'écran
   SystemChrome.setPreferredOrientations(
-    [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]
-  )
-  .then((_) => 
-    // On désactive la barre de statut
-    SystemChrome.setEnabledSystemUIOverlays([])
-  )
-  .then((_) => 
-    // On lance le jeu
-    runApp(Chronochroma.createGame())
-  );
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .then((_) =>
+          // On désactive la barre de statut
+          SystemChrome.setEnabledSystemUIOverlays([]))
+      .then((_) =>
+          // On lance le jeu
+          runApp(const MyApp()));
 }
-
-
---
+++ chronochroma/lib/upgrade.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Améliorer mon personnage'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        // add a image on top of joystick which will move according to joystick movement
+        child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 50,
+              width: 50,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+}
