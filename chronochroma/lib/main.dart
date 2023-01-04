9579264b2e3cda4fcd96ccbb7d179b5c8049187a
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-01-04 15:07:58 +0100

    fix(main)

--- chronochroma/lib/main.dart
+++ chronochroma/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:flame/game.dart';
 import './chronochroma.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'upgrade.dart';
+import './helpers/controller.dart';
 
 void main() {
+  // On crée une instance du jeu
+  final game = Chronochroma();
   // On s'assure que le binding est initialisé
   WidgetsFlutterBinding.ensureInitialized();
   // On désactive la rotation de l'écran
@@ -11,8 +14,24 @@ void main() {
           [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
       .then((_) =>
           // On désactive la barre de statut
-          SystemChrome.setEnabledSystemUIOverlays([]))
-      .then((_) =>
-          // On lance le jeu
-          runApp(const MyApp()));
+          SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+              overlays: []))
+      .then((_) => {
+            // On lance le jeu
+            runApp(MaterialApp(
+                debugShowCheckedModeBanner: false,
+                home: Scaffold(
+                  body: Stack(
+                    children: [
+                      GameWidget(game: game),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Controller(
+                          onDirectionChanged: game.onArrowKeyChanged,
+                        ),
+                      ),
+                    ],
+                  ),
+                )))
+          });
 }
