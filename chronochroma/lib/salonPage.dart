b46e6c214b0ee1909185213908eb213b9df0c501
Lhiikan <antoine.chevaleyre54@gmail.com>
2023-02-06 17:35:13 +0100

    feat(dart): page du salon

--- chronochroma/lib/salonPage.dart
+++ chronochroma/lib/salonPage.dart
@@ -13,53 +13,55 @@ class _SalonPageState extends State<SalonPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/bg.jpg'),
-              fit: BoxFit.fill,
-            ),
-          ),
-          // add a image on top of joystick which will move according to joystick movement
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 20),
-                padding: EdgeInsets.only(left: 20, right: 20),
-                width: MediaQuery.of(context).size.width,
-                child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(children: [
-                        ElevatedButton(
-                            onPressed: () {
-                              Navigator.popAndPushNamed(context, '');
-                            },
-                            child: const Text("Tableau des scores")),
-                      ]),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            ElevatedButton(
-                                onPressed: () {
-                                  Navigator.popAndPushNamed(
-                                      context, '/upgrade');
-                                },
-                                child: const Text("Améliorations")),
-                            SizedBox(width: 10),
-                            ElevatedButton(
-                                onPressed: () {
-                                  Navigator.popAndPushNamed(context, '/game');
-                                },
-                                child: const Text("Jouer")),
-                          ]),
-                    ]),
+        body: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/bg_1.png'),
+          fit: BoxFit.cover,
+          scale: 2.0,
+        ),
+      ),
+      // add a image on top of joystick which will move according to joystick movement
+      child: Stack(children: [
+        Positioned(
+            top: 20,
+            left: 20,
+            child: CircleAvatar(
+              radius: 30,
+              backgroundImage: NetworkImage(
+                'https://source.unsplash.com/50x50/?portrait',
               ),
-            ],
-          )),
-    );
+            )),
+        Positioned(
+          bottom: 20,
+          left: 20,
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.popAndPushNamed(context, '');
+              },
+              child: const Text("Tableau des scores")),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 100,
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.popAndPushNamed(context, '/upgrade');
+              },
+              child: const Text("Améliorations")),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 20,
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.popAndPushNamed(context, '/game');
+              },
+              child: const Text("Jouer")),
+        ),
+      ]),
+    ));
   }
 }
