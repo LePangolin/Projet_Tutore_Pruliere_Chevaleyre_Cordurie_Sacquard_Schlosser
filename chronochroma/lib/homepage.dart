ac469cff6e4832bbd932b4b8ff646057ba00fa06
LePangolin <91209500+LePangolin@users.noreply.github.com>
2023-02-13 16:29:14 +0100

    feat(connexion): ajout bouton connexion

--- chronochroma/lib/homepage.dart
+++ chronochroma/lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,24 +11,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool connexion = false;
+
   @override
   Widget build(BuildContext context) {
+    isConnected();
     return Scaffold(
       body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/bg_1.png'),
-              fit: BoxFit.cover,
-              scale: 2.0,
-            ),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg_1.png'),
+            fit: BoxFit.cover,
+            scale: 2.0,
           ),
-          // add a image on top of joystick which will move according to joystick movement
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
+        ),
+        // add a image on top of joystick which will move according to joystick movement
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
               margin: const EdgeInsets.only(top: 40),
               child: Image(
                 image: const AssetImage('assets/images/logoMEILLEUREVER.png'),
@@ -48,12 +52,51 @@ class _MyHomePageState extends State<MyHomePage> {
                       },
                     ),
                   ),
+                  connexion
+                      ? Align(
+                          child: Container(
+                            margin: const EdgeInsets.only(right: 20, top: 60),
+                            padding: const EdgeInsets.only(bottom: 60),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.transparent),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(9.0),
+                                          side: const BorderSide(
+                                              color: Colors.white)))),
+                              child: const Text('Connexion',
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                  )),
+                              onPressed: () {},
+                            ),
+                          ))
+                      : Container(),
                 ],
               ),
-            )
-            ],
-          )),
-
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<void> isConnected() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? user = prefs.getString('user');
+    if (user == null) {
+      setState(() {
+        connexion = true;
+      });
+    } else {
+      setState(() {
+        connexion = false;
+      });
+    }
   }
 }
