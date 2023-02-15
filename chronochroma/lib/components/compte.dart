4c31adecdeb7380675b497a408bcd23782256e0e
LePangolin <91209500+LePangolin@users.noreply.github.com>
2023-02-15 14:31:59 +0100

    feat(compte):ajout token

--- chronochroma/lib/components/compte.dart
+++ chronochroma/lib/components/compte.dart
@@ -20,8 +20,17 @@ class Compte {
 
   int? _persoVueMax;
 
-  Compte._(this._pseudo, this._avatarUrl, this._score, this._persoVieMax,
-      this._persoVitesseMax, this._persoForceMax, this._persoVueMax);
+  String? _token;
+
+  Compte._(
+      this._pseudo,
+      this._avatarUrl,
+      this._score,
+      this._token,
+      this._persoVieMax,
+      this._persoVitesseMax,
+      this._persoForceMax,
+      this._persoVueMax);
 
   static Future<Compte?> getInstance() async {
     if (_instance == null) {
@@ -33,6 +42,7 @@ class Compte {
             json['data']['pseudo'],
             json['data']['avatar_url'],
             json['data']['score'],
+            json['data']["token"],
             json['data']["personnage"]["santeMax"],
             json['data']["personnage"]["vitesseMax"],
             json['data']["personnage"]["forceMax"],
@@ -56,6 +66,7 @@ class Compte {
           json['data']['pseudo'],
           json['data']['avatar_url'],
           json['data']['score'],
+          json['data']["token"],
           json['data']["personnage"]["santeMax"],
           json['data']["personnage"]["vitesseMax"],
           json['data']["personnage"]["forceMax"],
@@ -82,11 +93,11 @@ class Compte {
       _instance!._avatarUrl = avatar;
       var response = await http.post(
           Uri.parse("http://serverchronochroma.alwaysdata.net/user/avatar"),
-          body: {"pseudo": _instance!._pseudo, "avatar": avatar});
-      Map<String, dynamic> json = jsonDecode(response.body);
+          body: {"token": _instance!._token, "avatar": avatar});
       if (response.statusCode < 200 || response.statusCode > 299) {
         return false;
       } else {
+        Map<String, dynamic> json = jsonDecode(response.body);
         SharedPreferences prefs = await SharedPreferences.getInstance();
         prefs.setString('account', response.body);
         return true;
@@ -101,6 +112,8 @@ class Compte {
   String? get avatarUrl => _avatarUrl;
 
   int? get score => _score;
+
+  String? get token => _token;
 
   int? get persoVieMax => _persoVieMax;
 
